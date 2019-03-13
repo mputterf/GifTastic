e9651e0fb759f0bd8f8812246be907d4e3003844
Michael Putterflam <michael.putterflam@gmail.com>
2019-03-12 17:11:19 -0700

    changed variable names

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,45 +1,46 @@
-var buttonArr = ["dog", "cat"];
+var topics = ["dog", "cat"];
 
-makeButtons(buttonArr);
+// make buttons of the inital items in the array
+makeButtons(topics);
 
 $("#submit-button").on("click", function(){
     //Get input (and make lower case to avoid duplicate entries)
-    var newAnimal = $("#animal-input").val().toLowerCase();
+    var newtopic = $("#topic-input").val().toLowerCase();
 
     //send it to the array function and to the make the buttons
-    addToArr(newAnimal);
-    makeButtons(buttonArr);
+    addToArr(newtopic);
+    makeButtons(topics);
 
     // Clear the text field
-    $("#animal-input").val("");
+    $("#topic-input").val("");
 });
 
-function addToArr(newAnimal){
-    //check if animal is already in the array
-    if(!buttonArr.includes(newAnimal)){
-        // push to the new animal to the array
-        console.log(newAnimal);
-        buttonArr.push(newAnimal);
-        console.log(buttonArr);
+function addToArr(newtopic){
+    //check if topic is already in the array
+    if(!topics.includes(newtopic)){
+        // push to the new topic to the array
+        console.log(newtopic);
+        topics.push(newtopic);
+        console.log(topics);
     }
 
 }
 
-function makeButtons(buttonArr){
+function makeButtons(topics){
     // Clear the div first to prevent duplicate buttons
-    $("#animal-buttons").empty();
+    $("#topic-buttons").empty();
 
-    for(var i = 0; i<buttonArr.length; i++){
+    for(var i = 0; i<topics.length; i++){
         // Create the button from the array
-        var animalButton = $("<button>");
-        // Give it an id based on the animal
-        animalButton.attr("id", buttonArr[i] + "-button");
+        var topicButton = $("<button>");
+        // Give it an id based on the topic
+        topicButton.attr("id", topics[i] + "-button");
         // Give it a value for the ajax call
-        animalButton.val(buttonArr[i]);
+        topicButton.val(topics[i]);
         // Give it text so the user knows what they are clicking on
-        animalButton.text(buttonArr[i]);
+        topicButton.text(topics[i]);
         // Append to the DOM
-        $("#animal-buttons").append(animalButton);
+        $("#topic-buttons").append(topicButton);
         
     }
 }
